0c9ac240d7d1f61607401972aa2553e031a3e0d8
Andy Gómez-sw <andy.comlove@gmail.com>
2026-06-26 23:00:03 -0600

    BIENVENID@ 19

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPxXw8pCaubDU0ynSCW7tGYfAvlVVqvgM",
+    authDomain: "biblioweb-1efc6.firebaseapp.com",
+    projectId: "biblioweb-1efc6",
+    storageBucket: "biblioweb-1efc6.firebasestorage.app",
+    messagingSenderId: "688727975714",
+    appId: "1:688727975714:web:7cf8556eddc5fbeb10e77b",
+    measurementId: "G-Y8JHDHN33N"
+};
